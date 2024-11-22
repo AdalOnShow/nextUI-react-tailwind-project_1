7a14512f83d00820204a0109d1b04c60be96370c
AdalOnShow <mdadalbadsha2008@gmail.com>
2024-11-22 20:51:48 +0600

    ready vision section

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import Header from "./components/Navbar"
 import Hero from './sections/Hero';
+import Vision from "./sections/Vision";
 
 function App() {
   return (
-    <>
+    <main className="space-y-[128px]">
       <Header />
       <div className="flex justify-end items-center pl-20 lg:pl-16 xl:pl-64">
         <Hero />
       </div>
-    </>
+      <Vision />
+    </main>
   )
 }
 
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -2,16 +2,16 @@ import { Button } from '@nextui-org/react'
 
 const Hero = () => {
   return (
-    <section className="flex flex-col-reverse sm:flex-row gap-10 lg:gap-2.5 xl:gap-8 justify-between items-center mt-10 lg:mt-20 xl:mt-[128px]">
+    <section className="flex flex-col-reverse sm:flex-row gap-10 lg:gap-2.5 xl:gap-8 justify-between items-center">
       <div className="space-y-6 sm:w-1/2">
         <h1 className="font-bold leading-tight text-2xl md:text-3xl lg:text-4xl xl:text-5xl">Spend your Cardano <br className='hidden sm:block' /> anywhere, anytime.</h1>
         <p className="text-secondary w-4/5 text-xs lg:text-sm xl:text-base">Our user-friendly platform enables businesses and individuals to seamlessly convert and spend their crypto for everyday purchases.</p>
         <Button variant='solid' color='primary' radius='full'>
-          Get Started 
+          Get Started
           <img src="/right-arrow.svg" alt="arrow" className='w-[22.5px] h-[16.5px]' />
         </Button>
       </div>
-        <img src="/hero.svg" alt="hero image" className="max-w-[800px] w-10/12 sm:w-1/2" />
+      <img src="/hero.svg" alt="hero image" className="max-w-[800px] w-10/12 sm:w-1/2" />
     </section>
   )
 }
